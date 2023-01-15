72a41906bb083808f1bb776cbe2b5a9b9bf39750
AJ Tran <ajtrandev@gmail.com>
2023-01-14 22:15:37 -0500

    Fix collapse button in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,29 @@
+import "bootstrap/js/src/collapse.js";
+
 function Navbar() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+        <div className="container-fluid">
           <a className="navbar-brand" href='/'>Electric Nobody</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
-          <ul className="navbar-nav collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <li className="nav-item">
-              <a className="nav-link" href='/#/preface'>Preface</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href='/#/acknowledgement'>Acknowledgement</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href='https://troubledepartment.com/?product=bodyelectronic-poems-by-aerik-francis-paperback-preorder' target="_blank" rel="noreferrer">Buy the E-Book</a>
-            </li>
-          </ul>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href='/#/preface'>Preface</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href='/#/acknowledgement'>Acknowledgement</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href='https://troubledepartment.com/?product=bodyelectronic-poems-by-aerik-francis-paperback-preorder' target="_blank" rel="noreferrer">Buy the E-Book</a>
+              </li>
+            </ul>
           </div>
+        </div>
       </nav>
     </header>
   )
